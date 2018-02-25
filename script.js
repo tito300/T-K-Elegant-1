c8b043a263564014ccbc29ee8623a14bedd800ba
tito300 <34977986+tito300@users.noreply.github.com>
2018-02-24 20:53:25 -0600

    project-files
    uploaded project file from computer

---
+++ script.js
@@ -0,0 +1,12 @@
+let text = document.querySelector(".content").innerText.split("");
+console.log(text);
+
+// text.forEach( c => {
+
+//     c.addEventListener("mouseover", animateLetter);
+
+//     function animateLetter(e) {
+
+//         console.log("hi");
+//     }
+// })
