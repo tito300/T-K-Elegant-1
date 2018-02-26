afac5d45ae53d138dc728a3cf5582e354c7475bd
tito300 <tarek.demachkie@gmail.com>
2018-02-26 02:06:18 -0600

    Added btn and scroll down arrow

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const img = document.querySelector(".img");
 const imgH2 = document.querySelector("h2");
 const imgP = document.querySelector(".text");
+const imgArrow = document.querySelector(".arrow");
+const imgScroll = document.querySelector(".scroll");
 
 
 
@@ -12,9 +14,9 @@ function animateDom(e) {
 
     let offset = (imgH2.getBoundingClientRect().top - window.innerHeight) / 9;
 
-    console.log(offset);
+    // console.log(window.scrollY);
 
-        if (imgH2.getBoundingClientRect().top - window.innerHeight <= -60) {
+        if (imgH2.getBoundingClientRect().top - window.innerHeight <= -100) {
 
             imgH2.classList.add("slidein");
             imgP.classList.add("slidein");
@@ -28,5 +30,15 @@ function animateDom(e) {
         if ( imgH2.getBoundingClientRect().top - window.innerHeight <= 0) {
 
             img.style.backgroundPositionY = 100 + offset + "px";
+        }
+
+        if (window.scrollY > 10) {
+            // debugger;
+            // console.log(imgArrow);
+            imgArrow.style.display = "none";
+            imgScroll.style.display = "none";
+        } else if (window.scrollY < 10) {
+
+           setTimeout(()=> {imgArrow.style.display = "block";  imgScroll.style.display = "block"}, 1000)
         }
         }
