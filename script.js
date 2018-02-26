9ffcf734497d6540fd4259e53d26f4567ee7d96f
tito300 <tarek.demachkie@gmail.com>
2018-02-25 20:52:09 -0600

    animation added to 2 part

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-let text = document.querySelector(".content").innerText.split("");
-console.log(text);
+const img = document.querySelector(".img");
+const imgH2 = document.querySelector("h2");
+const imgP = document.querySelector(".text");
 
-// text.forEach( c => {
 
-//     c.addEventListener("mouseover", animateLetter);
 
-//     function animateLetter(e) {
 
-//         console.log("hi");
-//     }
-// })
+window.addEventListener("scroll", animateDom);
+
+function animateDom(e) {
+
+
+    let offset = (imgH2.getBoundingClientRect().top - window.innerHeight) / 9;
+
+    console.log(offset);
+
+        if (imgH2.getBoundingClientRect().top - window.innerHeight <= -60) {
+
+            imgH2.classList.add("slidein");
+            imgP.classList.add("slidein");
+            
+        } else if (imgH2.getBoundingClientRect().top - window.innerHeight >= 20) {
+
+            imgH2.classList.remove("slidein");
+            imgP.classList.remove("slidein");
+        }
+
+        if ( imgH2.getBoundingClientRect().top - window.innerHeight <= 0) {
+
+            img.style.backgroundPositionY = 100 + offset + "px";
+        }
+        }
